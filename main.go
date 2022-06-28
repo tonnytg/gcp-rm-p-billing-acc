4c1d6fc477229c11dae33c077dda0e8f7b39458c
Antonio Thomacelli <tonnytg@gmail.com>
2022-06-28 00:19:36 -0300

    feat: disable billingAccount project

--- main.go
+++ main.go
@@ -1,63 +1,35 @@
 package main
 
 import (
-	"cloud.google.com/go/pubsub"
 	"context"
-	"fmt"
 	"google.golang.org/api/cloudbilling/v1"
 	"google.golang.org/api/googleapi"
+	"google.golang.org/api/option"
 	"log"
 	"os"
 )
 
 func main() {
 	ctx := context.Background()
-	project_b_acc := os.Getenv("GCP_PROJECT_B_ACC")
-	project := os.Getenv("GCP_PROJECT_ID")
-	if project == "" {
-		log.Panic("PROJECT_ID empty")
-		return
-	}
 
 	p := cloudbilling.ProjectBillingInfo{
-		BillingAccountName: project_b_acc,
+		BillingAccountName: "",
 		BillingEnabled:     false,
-		Name:               project,
-		ProjectId:          project,
+		Name:               "",
+		ProjectId:          "",
 		ServerResponse:     googleapi.ServerResponse{},
 		ForceSendFields:    nil,
 		NullFields:         nil,
 	}
 
-	cloudbillingService, err := cloudbilling.NewService(ctx)
+	cloudbillingService, err := cloudbilling.NewService(ctx, option.WithAPIKey(os.Getenv("GCP_API_TOKEN")))
 	if err != nil {
-		log.Panic("problem with billingService")
+		log.Fatal(err)
 	}
 
-	b := cloudbillingService.Projects.UpdateBillingInfo(project, &p)
-	fmt.Printf("%s\n", b)
-
-	log.Println(cloudbillingService.BasePath)
-
-	client, err := pubsub.NewClient(ctx, project)
+	b, err := cloudbillingService.Projects.UpdateBillingInfo("projects/remove-billing-acc", &p).Do()
 	if err != nil {
-		// TODO: Handle error.
+		log.Panic(err)
 	}
-
-	topicId := os.Getenv("GCP_PUBSUB_TOPIC")
-	topic := client.Topic(topicId)
-	defer topic.Stop()
-	var results []*pubsub.PublishResult
-	r := topic.Publish(ctx, &pubsub.Message{
-		Data: []byte("hello world"),
-	})
-	results = append(results, r)
-	// Do other work ...
-	for _, r := range results {
-		id, err := r.Get(ctx)
-		if err != nil {
-			// TODO: Handle error.
-		}
-		fmt.Printf("Published a message with a message ID: %s\n", id)
-	}
+	log.Println(b)
 }
